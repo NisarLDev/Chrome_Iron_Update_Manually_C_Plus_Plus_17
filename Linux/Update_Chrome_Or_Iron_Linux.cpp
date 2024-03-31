3fe43c961740eab66a109ec8be2b109f544251e1
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-03-31 16:10:05 +0200

    Update Update_Chrome_Or_Iron_Linux.cpp

--- Linux/Update_Chrome_Or_Iron_Linux.cpp
+++ Linux/Update_Chrome_Or_Iron_Linux.cpp
@@ -3,16 +3,16 @@
 #include <string>
 #include <system_error>
 
+using namespace std;
 
 int main(){
-         using namespace std;
          string route;
          string destination;
          cout << "Directory path. Enter the location of the origin without quotes.: " << endl;
          cout << "To copy the files to linux, write the path without quotation marks, not singles and not doubles." << endl;
-         // Taking string input using getline() 
-         getline(cin, route); 
-         // Displaying string 
+         // Taking string input using getline()
+         getline(cin, route);
+         // Displaying string
          cout << "The location of the origin is : ";
          cout << route << endl;
          cout << "Directory path. Enter the location of the destination.: " << endl;
@@ -21,7 +21,6 @@ int main(){
          cout << "The location of the destination is : ";
          cout << destination << endl;
          const std::filesystem::path from{route}, to{destination};
- 
     try
     {
       	std::filesystem::copy(from, to); // throws: files do not exist
@@ -42,5 +41,5 @@ int main(){
 //     std::error_code ec;
 //     std::filesystem::copy_file(from, to, ec); // does not throw
 //     std::cout << "Non-throwing form sets error_code: " << ec.message() << '\n';
-     
+       return 0;
 };
